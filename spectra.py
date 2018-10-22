f6f7b75a04ab5f78963412b4889af99f506a9f95
Marthinus Spangenberg <mspangenberg.dev@gmail.com>
2018-10-22 10:13:39 +0200

    Bug hunting
    Used pylint to do some code cleanup

--- spectra.py
+++ spectra.py
@@ -1,13 +1,30 @@
+"""
+spectra - (MIT) Copyright (c) 2018 Marthinus Spangenberg
+
+A script that generates video colour palettes, written in Python!
+
+fudgeables:
+    step_size = # 1 to 2000, default is 500
+
+todo:
+    * test colour cluster-sampling
+    * reduce disk I/O with caching
+    * speed up with threading
+    * build simple GUI
+
+"""
+
 import os
-import glob
 import sys
-import cv2
+import glob
 import time
+import cv2
 import numpy as np
 from PIL import Image
 
 
 def main():
+    """pylint told me this had to be here"""
 
     class VideoInfo:
         """Generate information for a given video file"""
@@ -26,7 +43,6 @@ def main():
         def get_video():
             """find video file inside project folder"""
             filetypes = ['*.avi', '*.mkv', '*.mp4']
-            
             for filetype in filetypes:
                 for input_file in glob.glob(os.path.join(os.getcwd(), filetype)):
                     return input_file
@@ -36,7 +52,7 @@ def main():
             try:
                 duration = (self.framecount / self.fps)
                 return duration
-            except:
+            except ZeroDivisionError:
                 print('> Video file not found, please add one.')
                 quit()
 
@@ -44,12 +60,12 @@ def main():
             """generate needed folders if they don't exist"""
             try:
                 os.mkdir(os.path.join(self.current_directory, 'tmp'))
-            except:
+            except OSError:
                 print('> folders exist, moving on.')
 
         def calculate_frame_step(self):
             """get the gap in frames between each frame grab"""
-            frame_step = ( self.framecount / self.step_size )
+            frame_step = (self.framecount / self.step_size)
             step_gap = round(frame_step)
             return step_gap
 
@@ -58,11 +74,11 @@ def main():
             count = 0
             generated_image_count = 0
             active = True
-
             while active:
                 active, image = self.video_file.read()
                 if count % self.calculate_frame_step() == 0:
-                    cv2.imwrite(os.path.join(self.current_directory, 'tmp', 'frame%d.jpg' % count), image)
+                    cv2.imwrite(os.path.join(self.current_directory,\
+                     'tmp', 'frame%d.jpg' % count), image)
                     print('grabbing frame: ', generated_image_count)
                     generated_image_count += 1
                     if generated_image_count == self.step_size:
@@ -73,14 +89,15 @@ def main():
             """scale the frames down to 1px using bicubic interpolation"""
             pixel = (1, 1)
             for infile in glob.glob(os.path.join(self.current_directory, 'tmp', '*.jpg')):
-                im = Image.open(infile)
-                im.thumbnail(pixel, Image.ANTIALIAS)
-                im.save(infile)
-                im.close()
-                
+                image_file = Image.open(infile)
+                image_file.thumbnail(pixel, Image.ANTIALIAS)
+                image_file.save(infile)
+                image_file.close()
+
         def generate_pixel_line(self):
             """generate a pixel line from available pixels"""
-            image_list = map(Image.open, glob.glob(os.path.join(self.current_directory, 'tmp', '*.jpg')))
+            image_list = map(Image.open, glob.glob(os.path.join(self.current_directory,\
+             'tmp', '*.jpg')))
             imgs_comb = np.hstack(image_list)
             imgs_combs = Image.fromarray(imgs_comb)
             imgs_combs.save(os.path.join(self.current_directory, 'tmp', 'spectra_tmp.png'))
@@ -89,26 +106,27 @@ def main():
         def generate_spectra(self):
             """scale pixel line to final dimensions"""
             final_dimensions = 2000, 600
-            im = Image.open(os.path.join(self.current_directory, 'tmp', 'spectra_tmp.png'))
-            im_resized = im.resize(final_dimensions, Image.ANTIALIAS)
+            image_file = Image.open(os.path.join(self.current_directory, 'tmp', 'spectra_tmp.png'))
+            im_resized = image_file.resize(final_dimensions, Image.ANTIALIAS)
             im_resized.save("spectra_output.png")
-            im.close()
+            image_file.close()
 
         def housekeeping(self):
             """remove tmp directory"""
             try:
-                for tmpimage in glob.glob(os.path.join(self.current_directory, 'tmp', 'frame*.jpg')):
+                for tmpimage in glob.glob(os.path.join(self.current_directory,\
+                 'tmp', 'frame*.jpg')):
                     os.remove(tmpimage)
                 for tmpimage in glob.glob(os.path.join(self.current_directory, 'tmp', '*.png')):
                     os.remove(tmpimage)
                 os.rmdir(os.path.join(self.current_directory, 'tmp'))
-            except:
+            except OSError:
                 print('> The temporary directory or it\'s contents could not be removed')
 
         def printjob(self):
             """print job details to console"""
             output_time = time.time() - start_timer
-            print(f'filename: {self.input_file}\n\
+            print(f'\n| filename: {self.input_file}\n\
                 | resolution: {self.width}x{self.height}\n\
                 | duration: {self.duration:.2f} seconds\n\
                 | output: {output_time:.2f} seconds\n\
@@ -116,9 +134,9 @@ def main():
 
     # start timer
     start_timer = time.time()
-        
+
     # set frame grab step size
-    step_size = 2000
+    step_size = 500
 
     # initialize video file and get info
     video = VideoInfo(step_size)
@@ -136,4 +154,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
